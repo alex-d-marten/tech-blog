813fd37c297fbb5c4fc8facc57fd8e280e8cd957
amarten <alexdmarten@gmail.com>
2022-02-09 21:08:30 -0800

    added login and signup handlebars and routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -7,4 +7,14 @@ router.get('/', (req, res) => {
     res.render('homepage')
 })
 
+// login route
+router.get('/login', (req, res) => {
+    res.render('login');
+})
+
+// signup route
+router.get('/signup', (req, res) => {
+    res.render('signup');
+})
+
 module.exports = router;
