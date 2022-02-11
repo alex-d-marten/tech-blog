126318f8bd7ee728281da692237ba491d44375e3
amarten <alexdmarten@gmail.com>
2022-02-10 19:47:25 -0800

    added loggedIn boolean to generate login and logout buttons based on the value

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,12 +4,16 @@ const sequelize = require('../config/connection');
 
 // homepage route
 router.get('/', (req, res) => {
-    res.render('homepage')
+    res.render('homepage', {
+        loggedIn: req.session.loggedIn
+    })
 })
 
 // login route
 router.get('/login', (req, res) => {
-    res.render('login');
+    res.render('login', {
+        loggedIn: req.session.loggedIn
+    });
 })
 
 // signup route
