06937f82499f9094cd8edcfda58a9d8afd406bb9
amarten <alexdmarten@gmail.com>
2022-02-12 10:24:14 -0800

    setup all post routes, added the homepage route to render a list of current posts

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,135 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../../models');
+const sequelize = require('../../config/connection');
+
+// GET /api/posts
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(data => {
+        const posts = data.map(post => post.get({ plain: true }))
+        res.render('homepage', { posts })
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err)
+    })
+})
+
+// GET /api/posts/:id
+router.get('/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(data => {
+        if(!data) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        res.json(data)
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err)
+    })
+});
+
+// CREATE a new post POST /api/posts
+router.post('/', (req, res) => {
+    Post.create({
+        title: req.body.title,
+        user_id: req.body.user_id
+    })
+    .then(data => res.json(data))
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
+})
+
+// update a posts title PUT /api/posts/:id
+router.put('/:id', (req, res) => {
+    Post.update(
+        {
+            title: req.body.title
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    )
+    .then(data => {
+        if(!data) {
+            res.status(404).json({ message: 'No posts found with this id' });
+            return;
+        }
+        res.json(data)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
+})
+
+// DELETE a post by id DELETE /api/posts/:id
+router.delete('/:id', (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(data => {
+        if(!data) {
+            res.status(404).json({ message: 'No posts found with this id' });
+            return;
+        }
+        res.json(data)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
+})
+
+module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,16 +1,54 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
 
 
 // homepage route
 router.get('/', (req, res) => {
-    res.render('homepage', {
-        loggedIn: req.session.loggedIn
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(data => {
+        const posts = data.map(post => post.get({ plain: true }))
+        console.log(posts)
+        res.render('homepage', { 
+            posts,
+            loggedIn: req.session.loggedIn 
+        })
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err)
     })
 })
 
+// GET post by id
+
+
 // login route
 router.get('/login', (req, res) => {
+    if(req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
     res.render('login', {
         loggedIn: req.session.loggedIn
     });
@@ -20,5 +58,7 @@ router.get('/login', (req, res) => {
 router.get('/signup', (req, res) => {
     res.render('signup');
 })
+
+
 
 module.exports = router;
